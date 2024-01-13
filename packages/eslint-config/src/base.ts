084ecc27efe8651ee15350fa7ce0f66178020639
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2024-01-13 09:37:37 +0800

    fix: fix rule

--- packages/eslint-config/src/base.ts
+++ packages/eslint-config/src/base.ts
@@ -3,14 +3,16 @@ import type { Linter } from 'eslint';
 const base: Linter.Config = {
   extends: ['alloy', 'alloy/typescript'],
   ignorePatterns: ['dist', 'build', 'out'],
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
   overrides: [
     {
       files: ['*.config.js'],
       rules: {
         '@typescript-eslint/no-require-imports': 'off',
         'import/no-commonjs': 'off',
-        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       },
     },
   ],
