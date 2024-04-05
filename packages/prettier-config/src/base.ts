ce857fc7e8c283933879c1fb6d05394721051ade
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2024-04-06 00:45:54 +0800

    chore(prettier-config): upgrade deps

--- packages/prettier-config/src/base.ts
+++ packages/prettier-config/src/base.ts
@@ -2,11 +2,11 @@ import type { Config } from 'prettier';
 
 const base: Config = {
   plugins: [
-    'prettier-plugin-multiline-arrays',
-    'prettier-plugin-organize-imports',
     'prettier-plugin-packagejson',
+    'prettier-plugin-organize-imports',
     'prettier-plugin-prisma',
     'prettier-plugin-tailwindcss',
+    'prettier-plugin-multiline-arrays',
   ],
   printWidth: 120,
   proseWrap: 'never',
