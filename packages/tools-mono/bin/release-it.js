d391c4743c5f33433ef324331ba23dba78c5fcbc
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-06-30 00:18:03 +0800

    fix(tools/mono): small fix

--- packages/tools-mono/bin/release-it.js
+++ packages/tools-mono/bin/release-it.js
@@ -10,7 +10,7 @@ const customConfigArg = args.find((arg) => arg.startsWith('--config='));
 const customConfig = customConfigArg ? customConfigArg.split('=')[1] : null;
 
 let finalConfig = defaultConfig;
-// 如果提供了自定义配置路径，则读取并解析配置文件
+
 if (customConfig === 'A') {
   finalConfig = configA;
 }
