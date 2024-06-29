2ae2ab314d642c9f5337f13e9c1edefe5db96090
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-06-30 00:15:53 +0800

    feat(tools/mono): add release-it config feature

--- packages/tools-mono/bin/release-it.js
+++ packages/tools-mono/bin/release-it.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-import 'release-it/bin/release-it.js';
+import releaseIt from 'release-it';
+import { configA, defaultConfig } from '../config/release-it.config.js';
+
+// 解析命令行参数
+// Example: npx release-it --config=A
+const args = process.argv.slice(2);
+const customConfigArg = args.find((arg) => arg.startsWith('--config='));
+const customConfig = customConfigArg ? customConfigArg.split('=')[1] : null;
+
+let finalConfig = defaultConfig;
+// 如果提供了自定义配置路径，则读取并解析配置文件
+if (customConfig === 'A') {
+  finalConfig = configA;
+}
+
+releaseIt(finalConfig);
