34c4614d564bcc0f519c0a67970e41d9a43a0a45
暮冬拾柒 <zhoubaijun17@outlook.com>
2025-06-03 02:25:26 +0800

    feat(eslint-config): 更新 Tailwind CSS 配置，添加新规则和同级依赖

--- packages/eslint-config/src/tailwind.ts
+++ packages/eslint-config/src/tailwind.ts
@@ -1,13 +1,25 @@
 import type { Linter } from 'eslint';
+import path from 'path';
 
 const tailwind: Linter.Config = {
   extends: ['plugin:tailwindcss/recommended'],
   settings: {
     tailwindcss: {
-      config: './tailwind.config.js',
-      whitelist: [],
+      config: path.join(process.cwd(), 'tailwind.config.js'),
+      whitelist: [
+        'van-[\\w-]+',
+        'custom-[\\w-]+',
+        'tabs--[\\w-]+',
+      ],
     },
+  },
+  rules: {
+    'tailwindcss/enforces-negative-arbitrary-values': 'error',
+    'tailwindcss/enforces-shorthand': 'error',
+    'tailwindcss/no-contradicting-classname': 'error',
+    'tailwindcss/no-custom-classname': 'warn',
+    'tailwindcss/no-unnecessary-arbitrary-value': 'error',
   },
 };
 
-export default tailwind;
+module.exports = tailwind;
