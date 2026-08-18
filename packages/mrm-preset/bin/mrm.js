3761def98f8f103fca70bd67126a3ccba03b3a6a
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-08-18 23:00:08 +0800

    chore: remove mrm-preset

--- packages/mrm-preset/bin/mrm.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-import '@wesvg/cli/bin/run';
--- packages/mrm-preset/src/gitignore/index.ts
+++
@@ -1,6 +0,0 @@
-import { template } from 'mrm-core'
-import { join } from 'path'
-
-module.exports = async () => {
-  template('.gitignore', join(__dirname, './_gitignore')).apply().save();
-}
